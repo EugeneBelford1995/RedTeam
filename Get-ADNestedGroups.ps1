4b4036353d9c05d7a374909ad6d8af967d36731d
EugeneBelford1995 <114684848+EugeneBelford1995@users.noreply.github.com>
2023-01-26 23:15:48 -0500

    Add files via upload

---
+++ Get-ADNestedGroups.ps1
@@ -0,0 +1,45 @@
+﻿#Get all recursive groups a user belongs.
+Function Get-ADUserNestedGroups
+{
+    Param
+    (
+        [string]$DistinguishedName,
+        [array]$Groups = @()
+    )
+
+    #Get the AD object, and get group membership.
+    $ADObject = Get-ADObject -Filter "DistinguishedName -eq '$DistinguishedName'" -Properties memberOf, DistinguishedName;
+    
+    #If object exists.
+    If($ADObject)
+    {
+        #Enummurate through each of the groups.
+        Foreach($GroupDistinguishedName in $ADObject.memberOf)
+        {
+            #Get member of groups from the enummerated group.
+            $CurrentGroup = Get-ADObject -Filter "DistinguishedName -eq '$GroupDistinguishedName'" -Properties memberOf, DistinguishedName;
+       
+            #Check if the group is already in the array.
+            If(($Groups | Where-Object {$_.DistinguishedName -eq $GroupDistinguishedName}).Count -eq 0)
+            {
+                #Add group to array.
+                $Groups +=  $CurrentGroup;
+
+                #Get recursive groups.      
+                $Groups = Get-ADUserNestedGroups -DistinguishedName $GroupDistinguishedName -Groups $Groups;
+            }
+        }
+    }
+
+    #Return groups.
+    Return $Groups;
+}
+ 
+#The user to check.
+$User = "<SamAccountName/DN/UserPrincipal>";
+
+#Get all groups.
+$Groups = Get-ADUserNestedGroups -DistinguishedName (Get-ADUser -Identity $User).DistinguishedName;
+
+#Output all groups.
+$Groups | Select-Object Name | Sort-Object -Property Name;
---
+++ Mishky's PowerView (Red Team version).ps1
@@ -0,0 +1,25 @@
+﻿#Run/import Get-ADNestedGroups.ps1 first! (Available from: http://blog.tofte-it.dk/powershell-get-all-nested-groups-for-a-user-in-active-directory/)
+Import-Module ActiveDirectory
+Import-Module .\Get-ADNestedGroups.ps1
+Set-Location AD:
+$ADRoot = (Get-ADDomain).DistinguishedName
+
+$Accounts = (Get-ADUserNestedGroups (Get-ADUser "$env:username" -Properties *).DistinguishedName).Name
+$MyGroups = $Accounts.ForEach{[regex]::Escape($_)} -join '|'
+$MyGroups2 = $MyGroups.Replace('\','')
+$AlsoCheck = "$env:username|Everyone|Authenticated Users|Domain Users"
+$ADCS_Objects = (Get-ADObject -Filter * -SearchBase $ADRoot).DistinguishedName
+$DangerousRights = "GenericAll|WriteDACL|WriteOwner|GenericWrite|WriteProperty|Self"
+$DangerousGUIDs = "1131f6aa-9c07-11d1-f79f-00c04fc2dcd2|1131f6ad-9c07-11d1-f79f-00c04fc2dcd2|00000000-0000-0000-0000-000000000000|00299570-246d-11d0-a768-00aa006e0529"
+$FishyGUIDs = "ab721a56-1e2f-11d0-9819-00aa0040529b|ab721a54-1e2f-11d0-9819-00aa0040529b"
+
+ForEach ($object in $ADCS_Objects)
+{
+$BadACE = (Get-Acl $object -ErrorAction SilentlyContinue).Access | Where-Object {(($_.IdentityReference -match $MyGroups2) -or ($_.IdentityReference -match $AlsoCheck)) -and (($_.ActiveDirectoryRights -match $DangerousRights) -or ((($_.ActiveDirectoryRights -like "*ExtendedRight*") -and (($_.ObjectType -match $DangerousGUIDs) -or ($_.ObjectType -match $FishyGUIDs))))) -and ($_.AccessControlType -eq "Allow")}
+
+If ($BadACE)
+{
+Write-Host "Object: $object" -ForegroundColor Red
+$BadACE
+}
+}
